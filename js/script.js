5d9b6ad4d91c082fa6d983f1452ee65d6bbe8fbc
Salimi Salim <eksperimentalism@gmail.com>
2017-08-07 16:17:42 +0800

    code cleanup

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function convertToOriginalDate(dateString){
-    var parts = dateString.split('-');
-
-    var combineParts = parts[2] + '-' + parts[1] + '-' + parts[0];
-
-    return combineParts;
-}
